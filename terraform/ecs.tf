51c29c859fea7954503bbe97164c3b29beee1e90
zhuwj <byzhubaiyuan@gmail.com>
2025-08-06 15:15:24 +0800

    Add AWS compute resources

---
+++ terraform/ecs.tf
@@ -0,0 +1,69 @@
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+  tags = {
+    Name = "${var.project_name}-cluster"
+  }
+}
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "${var.project_name}-api-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "${var.project_name}-api-container"
+      image     = "nginx:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000,
+          hostPort      = 8000
+        }
+      ]
+      secrets = [
+        {
+          name      = "POSTGRES_PASSWORD"
+          valueFrom = aws_secretsmanager_secret.db_password.arn
+        }
+      ]
+      environment = [
+        {
+          name  = "POSTGRES_USER"
+          value = "aegis"
+        },
+        {
+          name  = "POSTGRES_DB"
+          value = "aegis"
+        },
+        {
+          name  = "POSTGRES_SERVER"
+          value = aws_db_instance.main.address
+        },
+        {
+          name      = "OPENAI_API_KEY",
+          valueFrom = "arn:aws:secretsmanager:ap-southeast-1:215288576473:secret:aegis-ai/openai-api-key-ETtHXe"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "api" {
+  name            = "${var.project_name}-api-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.api.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.ecs_service.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_nat_gateway.main]
+}
